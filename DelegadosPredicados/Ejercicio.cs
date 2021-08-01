972c829c4502c5041d851fa5546fbf769251629b
Rony Stico <ronystico@gmail.com>
2021-08-01 02:29:40 -0400

    Ejercicio predicado delegado hecho

---
+++ DelegadosPredicados/Ejercicio.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegadosPredicados
+{
+    class Ejercicio
+    {
+        // Delegado que devuelva numeros primos
+        static bool EsPrimo(int numero)
+        {
+            for(int i = 0; i < numero; i++) if (numero % 2 == 0) return false;
+            return true;
+        }
+        public static void CalcularPrimo()
+        {
+            Predicate<int> predicatePrimo = new Predicate<int>(EsPrimo);
+
+            List<int> numeros = new List<int>();
+            numeros.AddRange(new int[] { 2, 3, 5, 6, 7, 76, 4 });
+
+            if (numeros.Exists(predicatePrimo)) {
+                Console.WriteLine("Hay al menos un número primo");
+                List<int> numerosprimos = numeros.FindAll(predicatePrimo);
+                foreach (int lol in numerosprimos)
+                {
+                    Console.WriteLine(lol);
+                }
+            } 
+            else Console.WriteLine("No hay un primo");
+        }
+    }
+}
---
+++ DelegadosPredicados/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DelegadosPredicados
+{
+    class Program
+    {
+        //static bool DamePares(int num)
+        //{
+        //    if (num % 2 == 0) return true;
+        //    else return false;
+        //}
+
+        static bool Existe(Personas personas)
+        {
+            if (personas.Edad >= 18) return true;
+            else return false;
+        }
+        static void Main(string[] args)
+        {
+
+            Ejercicio.CalcularPrimo();
+
+            ///////////////////////////////////////////////
+            //List<Personas> personas = new List<Personas>();
+
+            //Personas personas1 = new Personas();
+            //personas1.Nombre = "Juana";
+            //personas1.Edad = 8;            
+            //Personas personas2 = new Personas();
+            //personas2.Nombre = "María";
+            //personas2.Edad = 7;
+            //Personas personas3 = new Personas();
+            //personas3.Nombre = "Ana";
+            //personas3.Edad = 3;
+
+            //personas.AddRange(new Personas[] { personas1, personas2, personas3 });
+
+            //Predicate<Personas> predicate = new Predicate<Personas>(Existe);
+            //bool existe = personas.Exists(predicate);
+
+            //if (existe)
+            //{
+            //    Console.WriteLine("Hay mayores de edad");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No hay mayores de edad");
+            //}
+
+            ////////////////////////////////////////////////////////
+
+            //List<int> list = new List<int>();
+            //list.AddRange(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            //// Declaramos delegado predicado
+            //Predicate<int> predicate = new Predicate<int>(DamePares);
+            //List<int> numPares = list.FindAll(predicate);
+            //foreach (int num in numPares)
+            //{
+            //    Console.WriteLine(num);
+            //}
+        }
+    }
+    class Personas
+    {
+        private string nombre;
+        private int edad;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Edad { get => edad; set => edad = value; }
+    }
+}
