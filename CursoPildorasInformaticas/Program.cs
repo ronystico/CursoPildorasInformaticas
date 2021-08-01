2bdb7378d7b3cbf518064696c44018b09232b811
Rony Stico <ronystico@gmail.com>
2021-07-31 20:40:38 -0400

    Add project files.

---
+++ CursoPildorasInformaticas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CursoPildorasInformaticas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
