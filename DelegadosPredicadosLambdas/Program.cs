5494b9423e1652c9a7615453f8a5493d871b29b4
Rony Stico <ronystico@gmail.com>
2021-07-31 22:01:31 -0400

    Delegados listo

--- DelegadosPredicadosLambdas/Program.cs
+++ DelegadosPredicadosLambdas/Program.cs
@@ -4,9 +4,28 @@ namespace DelegadosPredicadosLambdas
 {
     class Program
     {
+        // Definicion del objeto delegado
+        delegate void ObjetoDelegado(string msjj);
         static void Main(string[] args)
         {
-            
+            ObjetoDelegado objetoDelegado = new ObjetoDelegado(MensajeBienvenida.SaludoBienvenida);
+            objetoDelegado("Hola tú");
+            objetoDelegado = new ObjetoDelegado(MensajeDespedida.SaludoDespedida);
+            objetoDelegado("Adiós tú");
+        }
+    }
+    class MensajeBienvenida
+    {
+        public static void SaludoBienvenida(string msj)
+        {
+            Console.WriteLine("Mensaje de bienvenida: {0}", msj);
+        }
+    }
+    class MensajeDespedida
+    {
+        public static void SaludoDespedida(string msj)
+        {
+            Console.WriteLine("Mensaje de despedida: {0}", msj);
         }
     }
 }
